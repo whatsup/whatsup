68135ca500b4c2ccffc800687f507631b33d24a3
Denis Churbanov <4urbanoff@gmail.com>
2020-11-12 11:22:23 +0300

    add list component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export { Emitter, EmitterOptions } from './emitter'
 export { fractal, Fractal, FractalOptions } from './fractal'
 export { fraction, Fraction, FractionOptions } from './fraction'
+export { list, List, ListOptions } from './list'
 export { factor } from './factor'
 export { tmp } from './temporary'
 export { Mutator } from './mutator'
-export { stream, live, exec } from './runners'
+export { stream, live } from './runners'
+ 
---
+++ src/list.ts
@@ -0,0 +1,97 @@
+import { Emitter } from './emitter'
+import { Fraction, FractionOptions } from './fraction'
+import { Tree } from './fork'
+
+export interface ListOptions extends FractionOptions {}
+
+export class List<T> extends Fraction<T[]> {
+    get items() {
+        return this.get()
+    }
+
+    async *collector(tree: Tree<T[]>) {
+        const { delegation } = this
+
+        for await (const items of super.collector(tree)) {
+            const result = [] as T[]
+
+            for (const item of items) {
+                if (delegation && item instanceof Emitter) {
+                    result.push(yield* item)
+                } else {
+                    result.push(item)
+                }
+            }
+
+            yield result
+        }
+    }
+
+    push(...items: T[]): number {
+        const newItems = this.items.slice()
+        const result = newItems.push(...items)
+        this.set(newItems)
+        return result
+    }
+
+    unshift(...items: T[]): number {
+        const newItems = this.items.slice()
+        const result = newItems.unshift(...items)
+        this.set(newItems)
+        return result
+    }
+
+    pop(): T | undefined {
+        const newItems = this.items.slice()
+        const result = newItems.pop()
+        this.set(newItems)
+        return result
+    }
+
+    shift(): T | undefined {
+        const newItems = this.items.slice()
+        const result = newItems.shift()
+        this.set(newItems)
+        return result
+    }
+
+    sort(compareFn?: (a: T, b: T) => number): this {
+        const newItems = this.items.slice()
+        newItems.sort(compareFn)
+        this.set(newItems)
+        return this
+    }
+
+    reverse(): this {
+        const newItems = this.items.slice()
+        newItems.reverse()
+        this.set(newItems)
+        return this
+    }
+
+    splice(start: number, deleteCount?: number): this
+    splice(start: number, deleteCount: number, ...items: T[]): this {
+        const newItems = this.items.slice()
+        newItems.splice(start, deleteCount, ...items)
+        this.set(newItems)
+        return this
+    }
+
+    delete(item: T) {
+        const index = this.items.indexOf(item)
+
+        if (index !== -1) {
+            this.deleteAt(index)
+        }
+    }
+
+    deleteAt(index: number) {
+        const newItems = this.items.slice()
+        newItems.splice(index, 1)
+        this.set(newItems)
+    }
+}
+
+export function list<T>(items: T[] = [], options?: ListOptions) {
+    return new List(items, options)
+}
