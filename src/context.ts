727a16b1578ae358ec42668d69771fd667886ff9
Denis Churbanov <4urbanoff@gmail.com>
2020-11-16 15:54:54 +0300

    add event system

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,17 @@
 import { Atom } from './atom'
 import { Factor } from './factor'
+import { Event, EventCtor, EventListener } from './event'
 
 export class Context {
     private readonly atom: Atom
+    private eventListeners!: Map<EventCtor, Set<EventListener>>
 
     constructor(atom: Atom) {
         this.atom = atom
+    }
+
+    update() {
+        return this.atom.update()
     }
 
     /** @internal */
@@ -22,7 +28,50 @@ export class Context {
         factor.set(this, value)
     }
 
-    update() {
-        return this.atom.update()
+    on<T extends Event>(ctor: EventCtor<T>, listener: EventListener<T>) {
+        if (!this.eventListeners) {
+            this.eventListeners = new Map()
+        }
+        if (!this.eventListeners.has(ctor)) {
+            this.eventListeners.set(ctor, new Set())
+        }
+
+        this.eventListeners.get(ctor)!.add(listener)
+
+        return () => this.off(ctor, listener)
+    }
+
+    off<T extends Event>(ctor: EventCtor<T>, listener?: EventListener<T>) {
+        if (this.eventListeners) {
+            if (listener) {
+                if (this.eventListeners.has(ctor)) {
+                    this.eventListeners.get(ctor)!.delete(listener)
+                }
+            } else {
+                this.eventListeners.delete(ctor)
+            }
+        }
+    }
+
+    dispath<T extends Event>(event: T) {
+        const ctor = event.constructor as EventCtor<T>
+
+        if (this.eventListeners && this.eventListeners.has(ctor)) {
+            const listeners = this.eventListeners.get(ctor)!
+
+            for (const listener of listeners) {
+                listener(event)
+
+                if (event.isPropagationImmediateStopped()) {
+                    return
+                }
+            }
+        }
+
+        const { parent } = this
+
+        if (parent && !event.isPropagationStopped()) {
+            parent.dispath(event)
+        }
     }
 }
---
+++ src/event.ts
@@ -0,0 +1,28 @@
+enum Propagation {
+    Default,
+    Stopped,
+    ImmediateStopped,
+}
+
+export type EventCtor<T extends Event = any> = new (...args: any[]) => T
+export type EventListener<T extends Event = any> = (event: T) => void
+
+export abstract class Event {
+    private propagation = Propagation.Default
+
+    stopPropagation() {
+        this.propagation = Propagation.Stopped
+    }
+
+    stopImmediatePropagation() {
+        this.propagation = Propagation.ImmediateStopped
+    }
+
+    isPropagationStopped() {
+        return this.propagation === Propagation.Stopped
+    }
+
+    isPropagationImmediateStopped() {
+        return this.propagation === Propagation.ImmediateStopped
+    }
+}
