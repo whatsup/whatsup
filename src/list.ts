65584fb5982a387ef4edf89a6fc451ae73e22ec6
Denis Churbanov <4urbanoff@gmail.com>
2020-11-25 10:54:16 +0300

    make list child of observable

--- src/list.ts
+++ src/list.ts
@@ -1,9 +1,9 @@
 import { Stream } from './stream'
-import { Fraction, FractionOptions } from './fraction'
+import { Observable } from './observable'
 
-export interface ListOptions extends FractionOptions {}
+export interface ListOptions {}
 
-export class List<T> extends Fraction<T[]> {
+export class List<T> extends Observable<T[]> {
     *spread() {
         const items = yield* this
         const result = [] as T[]
@@ -22,14 +22,14 @@ export class List<T> extends Fraction<T[]> {
     splice(start: number, deleteCount?: number): this
     splice(start: number, deleteCount: number, ...items: T[]): this
     splice(start: number, ...other: any[]): this {
-        const newItems = this.value.slice()
+        const newItems = this.get().slice()
         newItems.splice(start, ...other)
         this.set(newItems)
         return this
     }
 
     insert(...items: T[]): this {
-        const newItems = this.value.slice()
+        const newItems = this.get().slice()
         newItems.push(...items)
         this.set(newItems)
         return this
@@ -40,7 +40,7 @@ export class List<T> extends Fraction<T[]> {
     }
 
     delete(...items: T[]): this {
-        const newItems = this.value.slice()
+        const newItems = this.get().slice()
 
         for (const item of items) {
             const index = newItems.indexOf(item)
@@ -57,20 +57,20 @@ export class List<T> extends Fraction<T[]> {
     }
 
     sort(compareFn?: (a: T, b: T) => number): this {
-        const newItems = this.value.slice()
+        const newItems = this.get().slice()
         newItems.sort(compareFn)
         this.set(newItems)
         return this
     }
 
     reverse(): this {
-        const newItems = this.value.slice()
+        const newItems = this.get().slice()
         newItems.reverse()
         this.set(newItems)
         return this
     }
 }
 
-export function list<T>(items: T[] = [], options?: ListOptions) {
-    return new List(items, options)
+export function list<T>(items: T[] = []) {
+    return new List(items)
 }
