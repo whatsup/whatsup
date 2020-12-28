bcb278fdee0daad27df01d36292fb23384d461f2
Denis Churbanov <4urbanoff@gmail.com>
2020-12-28 15:41:39 +0300

    renames

--- src/atomizer.ts
+++ src/atomizer.ts
@@ -1,33 +1,31 @@
 import { Stream } from './stream'
 import { Atom } from './atom'
 
-const GLOBAL_MULTI_MAP = new WeakMap<Stream<any>, Atom>()
+const GLOBAL_MAP = new WeakMap<Stream<any>, Atom>()
 
 export class Atomizer {
-    static readonly multiMap = new WeakMap<Stream<any>, Atom>()
-
     private readonly root: Atom
-    private readonly multiMap: WeakMap<Stream<any>, Atom>
+    private readonly map: WeakMap<Stream<any>, Atom>
 
     constructor(root: Atom) {
         this.root = root
-        this.multiMap = new WeakMap()
+        this.map = new WeakMap()
     }
 
     get(stream: Stream<any>, multi: boolean): Atom {
         if (multi) {
-            if (!this.multiMap.has(stream)) {
+            if (!this.map.has(stream)) {
                 const atom = new Atom(stream, this.root.context)
-                this.multiMap.set(stream, atom)
+                this.map.set(stream, atom)
             }
 
-            return this.multiMap.get(stream)!
+            return this.map.get(stream)!
         }
 
-        if (!GLOBAL_MULTI_MAP.has(stream)) {
-            GLOBAL_MULTI_MAP.set(stream, new Atom(stream, null))
+        if (!GLOBAL_MAP.has(stream)) {
+            GLOBAL_MAP.set(stream, new Atom(stream, null))
         }
 
-        return GLOBAL_MULTI_MAP.get(stream)!
+        return GLOBAL_MAP.get(stream)!
     }
 }
