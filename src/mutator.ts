1119caaba2e1c3bca2bc8944af15ac4b890c9f0d
Denis Churbanov <4urbanoff@gmail.com>
2020-12-29 03:00:59 +0300

    add mutator func

--- src/mutator.ts
+++ src/mutator.ts
@@ -1,4 +1,15 @@
-
 export abstract class Mutator<T> {
     abstract mutate(prev?: T): T
 }
+
+export function mutator<T>(cb: (prev?: T) => T) {
+    return new (class extends Mutator<T> {
+        mutate(prev?: T) {
+            return cb(prev)
+        }
+    })()
+}
+/*
+const increment = mutator((n = 0) => n + 1)
+const decrement = mutator((n = 0) => n - 1)
+*/
