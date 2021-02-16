841dac93081c3086d07f52e0e7287e7d6d509562
Denis Churbanov <4urbanoff@gmail.com>
2021-02-16 20:21:30 +0300

    conse: add Delegation type inference

--- src/conse.ts
+++ src/conse.ts
@@ -1,12 +1,13 @@
 import { Context } from './context'
 import { transaction } from './scheduler'
 import { Cause } from './cause'
+import { Delegation } from './delegation'
 
 export class Conse<T> extends Cause<T> {
     private contexts = new Set<Context>()
-    private value: T
+    private value: T | Delegation<T>
 
-    constructor(value: T) {
+    constructor(value: T | Delegation<T>) {
         super()
         this.value = value
     }
@@ -27,7 +28,7 @@ export class Conse<T> extends Cause<T> {
         return this.value
     }
 
-    set(value: T) {
+    set(value: T | Delegation<T>) {
         this.value = value
 
         transaction(() => {
@@ -38,6 +39,6 @@ export class Conse<T> extends Cause<T> {
     }
 }
 
-export function conse<T>(value: T) {
+export function conse<T>(value: T | Delegation<T>) {
     return new Conse(value)
 }
