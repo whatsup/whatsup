2d05500c9d2293531aa3f9ac57be462b815ad274
Denis Churbanov <4urbanoff@gmail.com>
2021-02-17 16:56:58 +0300

    add `redirect` from `browser-pathname` to exports

--- packages/route/src/index.ts
+++ packages/route/src/index.ts
@@ -1,1 +1,2 @@
+export { redirect } from '@whatsup/browser-pathname'
 export { route, PATHNAME, DEFAULT_ROUTE_VALUE, RouteGeneratorFunc } from './route'
--- packages/route/test/exports.test.ts
+++ packages/route/test/exports.test.ts
@@ -1,8 +1,10 @@
-import { route, PATHNAME, DEFAULT_ROUTE_VALUE } from '../src/index'
+import { redirect } from '@whatsup/browser-pathname'
+import { route, PATHNAME, DEFAULT_ROUTE_VALUE, redirect as _redirect } from '../src/index'
 import { route as _route, PATHNAME as _PATHNAME, DEFAULT_ROUTE_VALUE as _DEFAULT_ROUTE_VALUE } from '../src/route'
 
 it('exports', () => {
     expect(route).toBe(_route)
     expect(PATHNAME).toBe(_PATHNAME)
     expect(DEFAULT_ROUTE_VALUE).toBe(_DEFAULT_ROUTE_VALUE)
+    expect(redirect).toBe(_redirect)
 })
