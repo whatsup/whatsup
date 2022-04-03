9e7d4b60e88e5d4518a83dc924b817eb2ccf2508
Denis Churbanov <4urbanoff@gmail.com>
2022-04-03 10:57:39 +0300

    move splitter to own file

--- src/component.ts
+++ src/component.ts
@@ -1,40 +1,18 @@
 import { Stream, Producer } from './stream'
 import { GET_CONSUMER } from './symbols'
-import { atom, Atom } from './atom'
-
-class Atomizer<T> {
-    private readonly map: WeakMap<Atom, Atom<T>>
-    private readonly producer: Producer<T>
-    private readonly thisArg: unknown
-
-    constructor(producer: Producer<T>, thisArg: unknown) {
-        this.map = new WeakMap()
-        this.producer = producer
-        this.thisArg = thisArg
-    }
-
-    get(consumer: Atom) {
-        if (!this.map.has(consumer)) {
-            const { producer, thisArg } = this
-            const dependency = atom(consumer.context, producer, thisArg) as Atom<T>
-
-            this.map.set(consumer, dependency)
-        }
-
-        return this.map.get(consumer)!
-    }
-}
+import { Atom } from './atom'
+import { Splitter } from './splitter'
 
 export class Component<T = unknown> extends Stream<T> {
-    private readonly atomizer: Atomizer<T>
+    private readonly splitter: Splitter<T>
 
     constructor(producer: Producer<T>, thisArg?: unknown) {
         super()
-        this.atomizer = new Atomizer(producer, thisArg)
+        this.splitter = new Splitter(producer, thisArg)
     }
 
     getAtomFor(consumer: Atom): Atom<T> {
-        return this.atomizer.get(consumer)!
+        return this.splitter.getFor(consumer)!
     }
 
     *[Symbol.iterator](): Generator<any, T, any> {
