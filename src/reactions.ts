07f99713665abc31a63cf3c3a7ad4e73a88283a9
Denis Churbanov <4urbanoff@gmail.com>
2022-04-07 15:09:15 +0300

    add reactions

---
+++ src/reactions.ts
@@ -0,0 +1,38 @@
+import { Producer } from './stream'
+import { createAtom } from './atom'
+import { action, transaction } from './scheduler'
+
+export type DataHandler<T> = (data: T, prevData?: T | undefined) => void
+export type ErrorHandler = (e: Error) => void
+
+export const reaction = <T>(producer: Producer<T>, onData?: DataHandler<T>, onError?: ErrorHandler) => {
+    let prev: T | undefined = undefined
+    const source = createAtom(producer)
+    const atom = createAtom(() => {
+        try {
+            const data = source.get()
+
+            if (onData) {
+                onData(data, prev)
+            }
+
+            prev = data
+        } catch (e) {
+            if (onError) {
+                onError(e as Error)
+            }
+        }
+    })
+
+    transaction((t) => t.addEntry(atom))
+
+    return () => atom.dispose()
+}
+
+export const autorun = <T>(producer: Producer<T>) => {
+    const atom = createAtom(producer)
+
+    transaction((t) => t.addEntry(atom))
+
+    return () => atom.dispose()
+}
