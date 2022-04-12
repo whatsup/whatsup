a87a5e7c6bfbb52e4ddb038de29b4e4be016894c
Denis Churbanov <4urbanoff@gmail.com>
2022-04-12 09:02:43 +0300

    update typings

--- src/atom.ts
+++ src/atom.ts
@@ -3,7 +3,7 @@ import { Mutator } from './mutator'
 import { isGenerator } from './utils'
 
 export type Payload<T> = T | Delegation<T> | Mutator<T>
-export type PayloadIterator<T> = Iterator<Payload<T>, Payload<T> | unknown, unknown>
+export type PayloadIterator<T> = Iterator<Payload<T> | never, Payload<T> | unknown, unknown>
 export type GnProducer<T> = () => PayloadIterator<T>
 export type FnProducer<T> = () => Payload<T>
 export type Producer<T> = GnProducer<T> | FnProducer<T>
