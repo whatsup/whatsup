9c5b30eec3173649561444f39d5fa46346b8044b
Denis Churbanov <4urbanoff@gmail.com>
2022-07-13 11:32:18 +0300

    update docs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,8 @@ const config = {
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'whatsup', // Usually your GitHub org/user name.
-    projectName: 'whatsup', // Usually your repo name.
+    projectName: 'whatsup.github.io', // Usually your repo name.
+    deploymentBranch: 'gh-pages',
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
