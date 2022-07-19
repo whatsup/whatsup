4e97a2d15a57935cc6674b3ae1a69d84bd5a476c
Denis Churbanov <4urbanoff@gmail.com>
2022-07-19 17:33:35 +0300

    update scopedName tpl on testing env

--- packages/babel-plugin-transform-cssx/src/utils.ts
+++ packages/babel-plugin-transform-cssx/src/utils.ts
@@ -1,4 +1,6 @@
 import genericNames from 'generic-names'
-import { IS_PRODUCTION } from './constants'
+import { IS_PRODUCTION, IS_TESTING } from './constants'
 
-export const generateScopedName = genericNames(IS_PRODUCTION ? '[hash:base64:8]' : '[name]__[local]--[hash:base64:5]')
+export const generateScopedName = genericNames(
+    IS_TESTING ? '[name]__[local]' : IS_PRODUCTION ? '[hash:base64:8]' : '[name]__[local]--[hash:base64:5]'
+)
