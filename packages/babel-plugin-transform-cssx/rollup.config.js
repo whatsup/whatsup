a56f95b90af7ce1628c1d474fc4299430ae59688
Denis Churbanov <4urbanoff@gmail.com>
2022-07-19 14:56:00 +0300

    update rollup

--- packages/babel-plugin-transform-cssx/rollup.config.js
+++ packages/babel-plugin-transform-cssx/rollup.config.js
@@ -25,6 +25,7 @@ export default [
             autoExternal(),
             resolve({
                 extensions,
+                modulesOnly: true,
             }),
             babel({
                 extensions,
