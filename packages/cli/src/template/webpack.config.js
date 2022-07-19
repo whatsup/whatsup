487158f225a075ceb14e37e8e5cd6244788ae469
Denis Churbanov <4urbanoff@gmail.com>
2022-07-19 17:26:00 +0300

    update cli template

--- packages/cli/src/template/webpack.config.js
+++ packages/cli/src/template/webpack.config.js
@@ -1,40 +1,35 @@
 const path = require('path')
-const isDev = process.env.NODE_ENV === 'development'
+const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {
+    getCssLoaderOptions,
+} = require('@whatsup/babel-plugin-transform-cssx/compat')
 
-module.exports = {
+const common = {
     entry: './src/bundle.tsx',
-
     output: {
         path: path.resolve('./dist'),
         filename: 'bundle.js',
         chunkFilename: 'chunk.[chunkhash].js',
         publicPath: '/',
     },
-
-    devtool: isDev ? 'inline-source-map' : 'source-map',
-
-    mode: process.env.NODE_ENV,
-
     resolve: {
         modules: [path.resolve('./src'), path.resolve('node_modules')],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
-
     resolveLoader: {
         modules: [path.resolve('node_modules')],
     },
-
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             inject: 'body',
-            title: '<% projectName %>',
+            title: 'examples',
             hash: true,
             cache: true,
         }),
     ],
-
     module: {
         rules: [
             {
@@ -46,13 +41,27 @@ module.exports = {
                 test: /\.(jpg|jpeg|png|svg)$/,
                 type: 'asset/resource',
             },
+        ],
+    },
+}
+
+const development = {
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
             {
                 test: /\.(css|scss|sass)$/i,
-                use: ['style-loader', '@whatsup/webpack-loader-css-components'],
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: getCssLoaderOptions(),
+                    },
+                    'sass-loader',
+                ],
             },
         ],
     },
-
     devServer: {
         port: 3000,
         historyApiFallback: true,
@@ -60,4 +69,35 @@ module.exports = {
         hot: true,
         open: true,
     },
+}
+
+const production = {
+    devtool: 'source-map',
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss|sass)$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: getCssLoaderOptions(),
+                    },
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+}
+
+module.exports = (_, args) => {
+    switch (args.mode) {
+        case 'development':
+            return merge(common, development)
+        case 'production':
+            return merge(common, production)
+        default:
+            throw new Error('No matching webpack configuration was found!')
+    }
 }
