5aae4c4efa85c9f023c8419d872ed16fadd9bf1c
Denis Churbanov <4urbanoff@gmail.com>
2022-07-23 12:26:29 +0300

    migrate to css:namespace

--- packages/cli/src/template/src/app/app.tsx
+++ packages/cli/src/template/src/app/app.tsx
@@ -2,25 +2,40 @@ import { observable } from 'whatsup'
 import { Div, H1, P, Main } from './app.css'
 import { Logo } from './logo/logo'
 
-const colors = ['#cddc39', '#e91e63', '#9c27b0', '#2196f3', '#00bcd4', '#4caf50', '#ffc107']
+const colors = [
+    '#cddc39',
+    '#e91e63',
+    '#9c27b0',
+    '#2196f3',
+    '#00bcd4',
+    '#4caf50',
+    '#ffc107',
+]
 
 export function* App() {
     const color = observable(colors[0])
 
     while (true) {
         yield (
-            <Main container>
-                <Div spacer />
+            <Main css:container>
+                <Div css:spacer />
                 <Logo color={color()} />
-                <H1 header>whatsup</H1>
-                <P description>A frontend framework for chillout-mode development 🥤</P>
-                <Div spacer />
-                <Div circles>
+                <H1 css:header>whatsup</H1>
+                <P css:description>
+                    A frontend framework for chillout-mode development 🥤
+                </P>
+                <Div css:spacer />
+                <Div css:circles>
                     {colors.map((c) => (
-                        <Div circle __color={c} onClick={() => color(c)} key={c} />
+                        <Div
+                            css:circle
+                            css:$color={c}
+                            onClick={() => color(c)}
+                            key={c}
+                        />
                     ))}
                 </Div>
-                <P clickMe>click me</P>
+                <P css:clickMe>click me</P>
             </Main>
         )
     }
